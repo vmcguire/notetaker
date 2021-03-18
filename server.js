0af670cb18e2e11a67253176fc8587bdeb8fa585
vmcguire <victor.mcguire@gmail.com>
2021-03-18 15:57:04 -0700

    added finishing app routes

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static("public"));
+
 const PORT = process.env.PORT || 3001;
 
 const { notes } = require("./data/notes");
@@ -54,7 +56,7 @@ function validateNote(note) {
   return true;
 }
 
-app.get("/notes", (req, res) => {
+app.get("/api/notes", (req, res) => {
   let results = notes;
   if (req.query) {
     results = filterByQuery(req.query, results);
@@ -62,7 +64,7 @@ app.get("/notes", (req, res) => {
   res.json(results);
 });
 
-app.get("/notes/:id", (req, res) => {
+app.get("/api/notes/:id", (req, res) => {
   const result = findById(req.params.id, notes);
   if (result) {
     res.json(result);
@@ -71,7 +73,7 @@ app.get("/notes/:id", (req, res) => {
   }
 });
 
-app.post("/notes", (req, res) => {
+app.post("/api/notes", (req, res) => {
   req.body.id = notes.length.toString();
   if (!validateNote(req.body)) {
     res.status(400).send("The note is not properly formatted.");
@@ -79,6 +81,14 @@ app.post("/notes", (req, res) => {
     const note = createNewNote(req.body, notes);
     res.json(note);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.listen(PORT, () => {
